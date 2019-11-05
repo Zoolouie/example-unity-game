5eb8919006adeed6621124d6fe67a90d53cce99c
ZanderLouie <zander@bugsplat.com>
2019-11-05 10:28:12 -0700

    Added second commit

---
+++ Assets/Scripts/DamageTile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTile : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other) {
+        RubyController controller = other.GetComponent<RubyController>();
+        if (controller) {
+            controller.ChangeHealth(-1);
+        }
+    }
+}
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,22 +4,62 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+    
+    public int maxHealth = 5;
+   
+    
+    public int health {  get { return currentHealth; }}
+    int currentHealth;
+    
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+    
+    
+    Rigidbody2D rigidbody2d;
+    
     // Start is called before the first frame update
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-      float horizontal = Input.GetAxis("Horizontal");
-      float vertical = Input.GetAxis("Vertical");
-      Debug.Log(horizontal);
-      Vector2 position = transform.position;
-      position.y = position.y + 0.1f * vertical;
-      position.x = position.x + 0.1f * horizontal;
-      transform.position = position;
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
